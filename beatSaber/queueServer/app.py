a4ddca9270a283d94152734189fc25bde759a454
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2021-07-19 14:22:39 +0800

    reduce self-spam by checking if update is needed

--- beatSaber/queueServer/app.py
+++ beatSaber/queueServer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, get_template_attribute
+from flask import Flask, render_template, get_template_attribute, request
 from flask_cors import CORS, cross_origin
 import json
 
@@ -8,17 +8,15 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 path = "E:/Steam/steamapps/common/Beat Saber/UserData/SRM/SongRequestQueue.dat"
 
-@app.route('/',methods=['GET'])
-def getQueueApp():
-    with open(path,'r') as f:
-        dat = json.load(f)
-        return render_template('queue.html',songs = dat)
-
 @app.route('/queue',methods=['GET'])
 @cross_origin()
 def getUpdatedQueue():
     with open(path,'r') as f:
         dat = json.load(f)
+        serverQueue = '&'.join(sorted(s['song']['key'] for s in dat))
+        clientQueue = request.args.get('current')
+        if(serverQueue == clientQueue): return 'NO CHANGE'
+
         return get_template_attribute('queue.html','getQueue')(dat)
         
 app.run(port=5050)
